50af014de677af9f89838cff5b7d1f40780bb0c3
ASmellyCat <Joy.J.Xu@outlook.com>
2023-10-23 15:45:45 -0700

    finished initialisation of register

--- api/index.js
+++ api/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require("mongoose");
 const app = express()
+const User = require('./models/User')
 
 app.use(cors())
+app.use(express.json());
 
-app.post('/register', (req, res) => {
-    res.json('test ok2');
+mongoose.connect('mongodb+srv://isjoyxu:nnuAzhCtReOR5KMe@cluster0.scoxqav.mongodb.net/?retryWrites=true&w=majority');
+
+app.post('/register', async(req, res) => {
+    const {username, password} = req.body
+    const userDoc = await User.create({username, password})
+    res.json(userDoc)
 })
 app.listen(4000)
 
+//nnuAzhCtReOR5KMe
