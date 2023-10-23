107eb92c1391b74635ad21b290a0b9384e8ea426
ASmellyCat <Joy.J.Xu@outlook.com>
2023-10-22 23:18:52 -0700

    finished client

---
+++ client/src/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return(
+        <div className="post">
+            <img className="post-image"
+                 src="https://img.freepik.com/free-vector/pixel-art-mystical-background_52683-87349.jpg"
+                 alt="Abstract Art"/>
+            <div>
+                <h2>Abstract Euphoria</h2>
+                <p className="info">
+                    <a href="" className="author">Joy</a>
+                    <time>2013-10-17 18:25</time>
+                </p>
+                <p className="summary">Submerge into the vivid realm of abstract feelings. Let each hue, shade, and tint
+                    whisper its intricate tale, revealing emotions that sway like the waves, engulfing the senses in a dance
+                    of chromatic wonder. Journey through this kaleidoscope of sentiments, as every color beckons, weaving
+                    stories of joy, melancholy, passion, and serenity.</p>
+            </div>
+        </div>
+    )}
