b12d47942fe60745faf8b536f965f229351b3a60
ASmellyCat <Joy.J.Xu@outlook.com>
2023-10-23 22:34:53 -0700

    logout, userContext finished

--- api/index.js
+++ api/index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 const bcrypt = require('bcryptjs')
 const app = express()
 const User = require('./models/User')
 const jwt = require('jsonwebtoken')
+const cookieParse = require('cookie-parser')
 
-const salt = bcrypt.genSaltSync(10);
+const salt = bcrypt.genSaltSync(10)
 const secret = 'easfewmklmlk423789feqr012'
 
 app.use(cors({credentials:true, origin:'http://localhost:3000'}))
 app.use(express.json());
+app.use(cookieParse())
+
 
 mongoose.connect('mongodb+srv://isjoyxu:nnuAzhCtReOR5KMe@cluster0.scoxqav.mongodb.net/?retryWrites=true&w=majority');
 
@@ -33,13 +36,27 @@ app.post('/login', async (req,res) => {
     if (passOk) {
         jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
             if (err) throw err
-            res.cookie('token', token).json('ok');
+            res.cookie('token', token).json({
+                id:userDoc._id,
+                username,
+            })
         })
     } else {
         res.status(400).json('wrong credentials')
     }
 })
 
+app.get('/profile', (req, res) => {
+    const {token} = req.cookies
+    jwt.verify(token, secret, {}, (err, info) => {
+        if (err) throw err
+        res.json(info)
+    })
+})
+
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok')
+})
 app.listen(4000)
 
 //nnuAzhCtReOR5KMe
--- client/src/Pages/LoginPage.js
+++ client/src/Pages/LoginPage.js
@@ -1,12 +1,14 @@
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {Navigate} from "react-router-dom";
+import {UserContext} from "../UserContext";
 
 export default function LoginPage() {
-    const [username,setUsername] = useState('');
-    const [password,setPassword] = useState('');
-    const [redirect, setRedirect] = useState(false);
+    const [username,setUsername] = useState('')
+    const [password,setPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
+    const {setUserInfo} = useContext(UserContext)
     async function login(ev) {
-        ev.preventDefault();
+        ev.preventDefault()
         const response = await fetch('http://localhost:4000/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
@@ -14,6 +16,10 @@ export default function LoginPage() {
             credentials: 'include'
         })
         if (response.ok) {
+            response.json().then(userInfo => {
+                setUserInfo(userInfo)
+                setRedirect(true)
+            })
             setRedirect(true)
         } else {
             alert('wrong credentials')
