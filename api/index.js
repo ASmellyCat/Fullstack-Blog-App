82f60632504bc4bac87b8b445a9d8250cbf6992d
ASmellyCat <Joy.J.Xu@outlook.com>
2023-10-23 21:32:16 -0700

    cookie and redirect finished

--- api/index.js
+++ api/index.js
@@ -4,9 +4,12 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs')
 const app = express()
 const User = require('./models/User')
+const jwt = require('jsonwebtoken')
 
 const salt = bcrypt.genSaltSync(10);
-app.use(cors())
+const secret = 'easfewmklmlk423789feqr012'
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}))
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://isjoyxu:nnuAzhCtReOR5KMe@cluster0.scoxqav.mongodb.net/?retryWrites=true&w=majority');
@@ -27,7 +30,14 @@ app.post('/login', async (req,res) => {
     const {username,password} = req.body
     const userDoc = await User.findOne({username})
     const passOk = bcrypt.compareSync(password, userDoc.password)
-    res.json(passOk)
+    if (passOk) {
+        jwt.sign({username, id: userDoc._id}, secret, {}, (err, token) => {
+            if (err) throw err
+            res.cookie('token', token).json('ok');
+        })
+    } else {
+        res.status(400).json('wrong credentials')
+    }
 })
 
 app.listen(4000)
