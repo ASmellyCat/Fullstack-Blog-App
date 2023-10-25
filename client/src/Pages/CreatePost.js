7d3f9e9fc746e0ea1916c2809750454eaf842292
ASmellyCat <Joy.J.Xu@outlook.com>
2023-10-24 22:14:08 -0700

    createPost finished

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -1,6 +1,7 @@
 import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import {useState} from "react";
+import {Navigate, redirect} from "react-router-dom";
 
 const modules = {
     toolbar: [
@@ -23,17 +24,24 @@ export default function CreatePost() {
     const [summary,setSummary] = useState('');
     const [content,setContent] = useState('');
     const [files, setFiles] = useState('');
-    function createNewPost(ev) {
+    const [redirect, setRedirect] = useState(false);
+    async function createNewPost(ev) {
         const data = new FormData();
         data.set('title', title);
         data.set('summary', summary);
         data.set('content', content);
         data.set('file', files[0]);
         ev.preventDefault();
-        fetch('http://localhost:4000/post', {
+        const response = await fetch('http://localhost:4000/post', {
             method: 'POST',
             body: data,
         })
+        if (response.ok) {
+            setRedirect(true);
+        }
+    }
+    if (redirect) {
+        return <Navigate to={'/'} />
     }
     return (
         <form onSubmit={createNewPost}>
